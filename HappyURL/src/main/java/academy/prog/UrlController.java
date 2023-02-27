ed655e7b189e92c5aef4cab360a5c9cf67448438
Etkel <118313778+Etkel@users.noreply.github.com>
2023-02-27 23:31:31 +0200

    Update UrlController.java

--- HappyURL/src/main/java/academy/prog/UrlController.java
+++ HappyURL/src/main/java/academy/prog/UrlController.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
